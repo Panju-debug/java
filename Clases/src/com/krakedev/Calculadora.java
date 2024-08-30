f0aee6e11d1d277b14f4b20559abe7fe9c55eec2
Panju-debug <pablovizuete13@gmail.com>
2024-08-30 07:47:58 -0500

    reto9

--- Clases/src/com/krakedev/Calculadora.java
+++ Clases/src/com/krakedev/Calculadora.java
@@ -16,7 +16,17 @@ public class Calculadora {
 		resultado= numero1*numero2;
 		return resultado;
 	}
-	public double dividir(double dividendo1, double dividendo) {
-		
+	public double dividir(double dividendo1, double dividendo2) {
+		double resultado;
+		resultado=dividendo1/dividendo2;
+		return resultado;
+	}
+	public double promediar (double valor1, double valor2, double valor3) {
+		double suma=valor1 + valor2 +valor3;
+		double resultado=suma/3;
+		return resultado;
+	}
+	public void mostrarResultado() {
+		System.out.println("Ahorita no joven, sali al almuerzo.Regreso en 5 minutos");
 	}
 }
--- Clases/src/com/krakedev/TestCalculadora.java
+++ Clases/src/com/krakedev/TestCalculadora.java
@@ -6,12 +6,25 @@ public class TestCalculadora {
 		Calculadora calcu;
 		int resultadoSuma;
 		int resultadoResta;
+		double resultadoProducto;
+		double resultadoDivision;
+		double resultadoPromedio;
+		
+		
 		
 		calcu=new Calculadora();
 		resultadoSuma=calcu.sumar(5, 3);
 		resultadoResta=calcu.restar(5, 3);
 		System.out.println("RESULTADO:"+ " "+resultadoSuma);
 		System.out.println("RESULTADO:"+" "+resultadoResta);	
+		
+		resultadoProducto=calcu.multiplicar(8.5,7.2);
+		resultadoDivision=calcu.dividir(11, 5.5);
+		resultadoPromedio=calcu.promediar(7.5,9.5,10);
+		System.out.println("RESULTADO:"+ " "+resultadoProducto);
+		System.out.println("RESULTADO:"+ " "+resultadoDivision);
+		System.out.println("RESULTADO:"+ " "+resultadoPromedio);
+		calcu.mostrarResultado();
 	}
 
 }
